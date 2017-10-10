04385b044c8745d837c738799268deaa356fd900
Nurtleu <nur71701@gmail.com>
2017-10-10 18:34:19 +0600

    homework

---
+++ WinFormsHW04.10/WinFormsHW04.10/Menu.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WinFormsHW04._10
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTopLevelMenu = new System.Windows.Forms.TextBox();
+            this.textBoxSubItem = new System.Windows.Forms.TextBox();
+            this.buttonLevelMenu = new System.Windows.Forms.Button();
+            this.buttonSubItem = new System.Windows.Forms.Button();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assemblyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mainMenuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxTopLevelMenu
+            // 
+            this.textBoxTopLevelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTopLevelMenu.Location = new System.Drawing.Point(3, 3);
+            this.textBoxTopLevelMenu.Multiline = true;
+            this.textBoxTopLevelMenu.Name = "textBoxTopLevelMenu";
+            this.textBoxTopLevelMenu.Size = new System.Drawing.Size(220, 40);
+            this.textBoxTopLevelMenu.TabIndex = 0;
+            // 
+            // textBoxSubItem
+            // 
+            this.textBoxSubItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSubItem.Location = new System.Drawing.Point(3, 14);
+            this.textBoxSubItem.Multiline = true;
+            this.textBoxSubItem.Name = "textBoxSubItem";
+            this.textBoxSubItem.Size = new System.Drawing.Size(220, 40);
+            this.textBoxSubItem.TabIndex = 1;
+            // 
+            // buttonLevelMenu
+            // 
+            this.buttonLevelMenu.BackColor = System.Drawing.Color.White;
+            this.buttonLevelMenu.Location = new System.Drawing.Point(247, 3);
+            this.buttonLevelMenu.Name = "buttonLevelMenu";
+            this.buttonLevelMenu.Size = new System.Drawing.Size(150, 40);
+            this.buttonLevelMenu.TabIndex = 2;
+            this.buttonLevelMenu.Text = "Добавить пункт меню";
+            this.buttonLevelMenu.UseVisualStyleBackColor = false;
+            this.buttonLevelMenu.Click += new System.EventHandler(this.ButtonLevelMenu_Click);
+            // 
+            // buttonSubItem
+            // 
+            this.buttonSubItem.BackColor = System.Drawing.Color.White;
+            this.buttonSubItem.Location = new System.Drawing.Point(248, 14);
+            this.buttonSubItem.Name = "buttonSubItem";
+            this.buttonSubItem.Size = new System.Drawing.Size(150, 40);
+            this.buttonSubItem.TabIndex = 3;
+            this.buttonSubItem.Text = "Добавить подменю";
+            this.buttonSubItem.UseVisualStyleBackColor = false;
+            this.buttonSubItem.Click += new System.EventHandler(this.ButtonSubItem_Click);
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.projectToolStripMenuItem,
+            this.assemblyToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(424, 24);
+            this.mainMenuStrip.TabIndex = 4;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.White;
+            this.buttonDelete.Location = new System.Drawing.Point(167, 214);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(190, 35);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.editToolStripMenuItem.Text = "Правка";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.viewToolStripMenuItem.Text = "Вид";
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.projectToolStripMenuItem.Text = "Проект";
+            // 
+            // assemblyToolStripMenuItem
+            // 
+            this.assemblyToolStripMenuItem.Name = "assemblyToolStripMenuItem";
+            this.assemblyToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.assemblyToolStripMenuItem.Text = "Сборка";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBoxTopLevelMenu);
+            this.panel1.Controls.Add(this.buttonLevelMenu);
+            this.panel1.Location = new System.Drawing.Point(12, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 63);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.buttonSubItem);
+            this.panel2.Controls.Add(this.textBoxSubItem);
+            this.panel2.Location = new System.Drawing.Point(12, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(399, 78);
+            this.panel2.TabIndex = 7;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 261);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.mainMenuStrip);
+            this.Name = "Menu";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTopLevelMenu;
+        private System.Windows.Forms.TextBox textBoxSubItem;
+        private System.Windows.Forms.Button buttonLevelMenu;
+        private System.Windows.Forms.Button buttonSubItem;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assemblyToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ WinFormsHW04.10/WinFormsHW04.10/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsHW04._10
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLevelMenu_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxTopLevelMenu.Text)) mainMenuStrip.Items.Add(textBoxTopLevelMenu.Text);
+        }
+
+        private void ButtonSubItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxSubItem.Text))fileToolStripMenuItem.DropDownItems.Add(textBoxSubItem.Text);
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            textBoxTopLevelMenu.Clear();
+            textBoxSubItem.Clear();
+        }
+    }
+}
